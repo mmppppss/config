f153b9a92d78335af1d7f7c08a03198043c8bf03
mmppppss <ppozosoliz@gmail.com>
2025-08-28 18:35:52 -0400

    fix bugs, add snippets nvim

--- nvim/lua/lazyconf.lua
+++ nvim/lua/lazyconf.lua
@@ -36,7 +36,11 @@ local plugins = {
 	{
 		"L3MON4D3/LuaSnip",
 		version = "v2.*",
-		dependencies = "rafamadriz/friendly-snippets"
+		build = "make install_jsregexp", -- opcional, mejora regex avanzada
+		dependencies = { "rafamadriz/friendly-snippets" },
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end
 	},
 	{
 		"lervag/vimtex",
@@ -89,28 +93,54 @@ local plugins = {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip",
 		},
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping.select_next_item(),
-					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+
+					-- Tab para navegar *y* expandir snippets
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
 					{ name = "buffer" },
+					{ name = "path" },
 				}),
 			})
 		end,
+
+
 	},
 	"preservim/nerdtree",
 
